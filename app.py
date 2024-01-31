c1e171ec5784d388eda2a44aea6a0aadc865aa66
DevilRange <58787282+DevilRange@users.noreply.github.com>
2024-01-31 02:57:29 +0200

    Update app.py - Use requests instead of selenium
    For memory efficiency and speed, the script has been rewritten to use requests instead of selenium.

--- app.py
+++ app.py
@@ -1,125 +1,201 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from urllib.request import urlopen
-from datetime import datetime
-import os, sys, schedule, time
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+import logging
+import random
+import re
+import os
+import sys
 
 
-username = os.environ.get('FL_USERNAME', None)
-password = os.environ.get('FL_PASSWORD', None)
-chrome_driver = os.environ.get('DRIVER', None)
-interval = os.environ.get('CHECK_INTERVAL', None)
-
-use_driver = "http://" + chrome_driver + ":3000/webdriver"
-
-if username is None:
-    print("Error: FL_USERNAME is not set.")
-    sys.exit(1) 
-
-if password is None:
-    print("Error: FL_PASSWORD is not set.")
-    sys.exit(1) 
-
-if int(interval) < 5 or interval is None:
-    print("Error: Set an interval more than 10 minutes. Don't hammer the website.")
-    sys.exit(1)
-
-def get_public_ip():
-    try:
-        myip = urlopen("http://ifconfig.me/ip")
-    except HTTPError as e:
-        print('Error code: ', e.code)
-        os.exit(1)
-    except URLError as e:
-        print('Reason: ', e.reason)
-        os.exit(1)
-    else:
-        return myip.read().decode('utf-8')
-
-def driver_init():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--headless")
-
-    driver = webdriver.Remote(
-        command_executor=use_driver,
-        options=chrome_options
-    )
-
-    driver.implicitly_wait(10)
-
-    return driver
-
-def get_fl_ip(driver):
-    driver.get('https://filelist.io/login.php?returnto=%2Fmy.php')
-    # print(driver.title)
-
-    username_field = driver.find_element(By.XPATH, '//*[@id="username"]')  # Find the search box
-    username_field.send_keys(username)
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[logging.StreamHandler()]
+)
 
 
-    password_field = driver.find_element(By.XPATH, '//*[@id="password"]')
-    password_field.send_keys(password + Keys.RETURN)
+def is_ipv4_address(ip):
+    return bool(re.match(r'^(\d{1,3}\.){3}\d{1,3}$', ip))
 
-    #driver.save_screenshot("screenshot.png")
 
-    redirected_url = driver.current_url
-    if redirected_url == "https://filelist.io/takelogin.php":
-        print("Check your filelist username and password. Check your compose file and recreate container")
+def authenticate(user, password):
+    session = requests.Session()
+    url = 'https://filelist.io/login.php'
+
+    while True:
+        try:
+            response = session.get(url)
+            break
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Error with {url}: {e}")
+            sleep(600)  # 10 minutes before retrying
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    validator = soup.find('input', {'name': 'validator'}).get('value', '') if soup.find('input',
+                                                                                        {'name': 'validator'}) else ''
+    data = {
+        "validator": validator,
+        "username": user,
+        "password": password,
+        "unlock": "1",
+        "returnto": "%252F",
+    }
+
+    login_url = 'https://filelist.io/takelogin.php'
+
+    while True:
+        try:
+            session.post(login_url, data=data)
+            if not session.cookies.get("pass"):
+                logging.error("Login not successful, check credentials...")
+                # 6 hour wait because wrong credentials can block the account.
+                sleep(21600)
+            return session
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Error with {login_url}: {e}")
+            sleep(600)  # 10 minutes before retrying
+
+
+def get_current_wan_ip():
+    urls = [
+        'https://checkip.amazonaws.com/',
+        'https://ipinfo.io/ip/',
+        'https://api.ipify.org',
+        'https://ifconfig.io/ip',
+        'https://icanhazip.com/'
+    ]
+
+    # Randomising which endpoint is used to not abuse.
+    random.shuffle(urls)
+
+    while True:
+        for url in urls:
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
+                logging.info(f"Got IP from {url}!")
+                ip = response.text.strip()
+                if is_ipv4_address(ip):
+                    return ip  # Return if successful
+                else:
+                    logging.warning(f"IP format is wrong - {ip}")
+            except requests.exceptions.RequestException as e:
+                logging.warning(f"Error with {url}: {e}")
+
+        logging.error("Cannot get WAN IP; possibly a connection issue, retrying in 5 minutes...")
+        sleep(300)
+
+
+def is_authenticated(session):
+    profile_page_url = 'https://filelist.io/my.php'
+    while True:
+        try:
+            response = session.get(profile_page_url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            # Check for the presence of the 'whitelistip' input field
+            whitelist_ip_field = soup.find('input', {'name': 'whitelistip'})
+
+            return whitelist_ip_field is not None
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Error with {profile_page_url}: {e}")
+        logging.error(f"Cannot get {profile_page_url}, retrying in 5 minutes...")
+        sleep(300)
+
+
+def fetch_profile_ip(session):
+    my_page_url = "https://filelist.io/my.php"
+
+    while True:
+        try:
+            response = session.get(my_page_url)
+            soup = BeautifulSoup(response.content, "html.parser")
+
+            whitelist_ip_field = soup.find('input', {'name': 'whitelistip'})
+            profile_ip = whitelist_ip_field.get('value', '') if whitelist_ip_field else ''
+
+            return profile_ip
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Error with {my_page_url}: {e}")
+        logging.error(f"Cannot get {my_page_url}, retrying in 5 minutes...")
+        sleep(300)
+
+
+def fetch_and_update_profile(session, current_wan_ip):
+    my_page_url = "https://filelist.io/my.php"
+    update_page_url = "https://filelist.io/takeprofedit.php"
+
+    while True:
+        try:
+            response = session.get(my_page_url)
+            soup = BeautifulSoup(response.content, "html.parser")
+
+            form_data = {}
+            for input_tag in soup.find_all('input'):
+                name = input_tag.get('name')
+                value = input_tag.get('value', '')
+                if name:
+                    form_data[name] = value
+
+            # Delete the resetpasskey tag from the dict to avoid resetting the passkey...
+            form_data.pop("resetpasskey", None)
+
+            if form_data.get('whitelistip') != current_wan_ip:
+                form_data['whitelistip'] = current_wan_ip
+                update_response = session.post(update_page_url, data=form_data)
+                if update_response.status_code == 200:
+                    logging.info("Successfully updated the whitelist IP.")
+                    break
+                else:
+                    logging.error(f"Failed to update. Status Code: {update_response.status_code}")
+            else:
+                logging.info("No update needed; WAN IP matches the Whitelist IP.")
+                break
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Error with updating whitelist IP: {e}")
+        logging.info(f"Retrying in 5 minutes...")
+        sleep(300)
+
+
+if __name__ == '__main__':
+    username = os.environ.get('FL_USERNAME', None)
+    password = os.environ.get('FL_PASSWORD', None)
+
+    if username is None:
+        logging.error("Error: FL_USERNAME is not set.")
         sys.exit(1)
 
-    #results = driver.find_element(By.XPATH, '//*[@id="maincolumn"]/div/div[5]/div/div[2]/form/fieldset[17]/label/input')
-    whitelist_field = driver.find_element(By.NAME, 'whitelistip')
-    fl_ip = whitelist_field.get_attribute('value')
+    if password is None:
+        logging.error("Error: FL_PASSWORD is not set.")
+        sys.exit(1)
 
-    return fl_ip
+    session = authenticate(username, password)
+    profile_ip = fetch_profile_ip(session)
+    current_wan_ip = get_current_wan_ip()
+    logging.info(f"Current WAN IP: {current_wan_ip}")
+    logging.info(f"Profile Whitelist IP: {profile_ip}")
 
-def set_fl_ip(ip, driver):
-    driver.get('https://filelist.io/login.php?returnto=%2Fmy.php')
-    # print(driver.title)
-
-    username_field = driver.find_element(By.XPATH, '//*[@id="username"]')  # Find the search box
-    username_field.send_keys(username)
-
-
-    password_field = driver.find_element(By.XPATH, '//*[@id="password"]')
-    password_field.send_keys(password + Keys.RETURN)
-
-    #results = driver.find_element(By.XPATH, '//*[@id="maincolumn"]/div/div[5]/div/div[2]/form/fieldset[17]/label/input')
-    whitelist_field = driver.find_element(By.NAME, 'whitelistip')
-    whitelist_field.clear()
-    whitelist_field.send_keys(ip)
-    
-    form = driver.find_element(By.NAME, "myForm")
-    form.submit()
-    
-    driver.save_screenshot("screenshot.png")
-    return
-
-def check_ip(driver):
-    public_ip = get_public_ip()
-    fl_ip = get_fl_ip(driver)
-
-    current_datetime = datetime.now()
-
-    if public_ip != fl_ip:
-        print("{} - Your public IP is {} and the one in FL profile is {}. Proceeding to change.".format(current_datetime, public_ip, fl_ip))
-        set_fl_ip(public_ip, driver)
+    # Initial check and update if necessary
+    if current_wan_ip != profile_ip:
+        logging.info("WAN IP does not match profile IP. Updating...")
+        fetch_and_update_profile(session, current_wan_ip)
     else:
-        print("{} - Your IP address is updated".format(current_datetime))
+        logging.info("WAN IP matches profile IP. No update needed.")
 
+    # Loop to check for IP changes
+    while True:
+        new_wan_ip = get_current_wan_ip()
+        if new_wan_ip != current_wan_ip:
+            logging.info(f"WAN IP changed to: {new_wan_ip}")
+            current_wan_ip = new_wan_ip
+            if not is_authenticated(session):
+                logging.warning("Session expired, re-authenticating...")
+                session = authenticate(username, password)
+            fetch_and_update_profile(session, current_wan_ip)
+        else:
+            logging.info("No change in WAN IP.")
 
-driver = driver_init()
-
-# Trigger an initial check - Be careful. You might get baned for an hour if you have your credentials wrong.
-# check_ip()
-
-print("Using {} driver. Waiting {} minute for the first check".format(use_driver, interval))
-
-# Schedule the task to run every x minutes
-schedule.every(10).minutes.do(check_ip(driver))
-
-while True:
-    schedule.run_pending()
-    time.sleep(2)
+        # Wait for 1 minute before checking again
+        sleep(60)
